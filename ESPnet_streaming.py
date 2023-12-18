f4afeb2a0a7c4e8c8502fd88dd0439348605c5a1
Sankalp-Mittal <sankalpm22@iitk.ac.in>
2023-12-18 18:17:50 +0530

    Add model loading

--- ESPnet_streaming.py
+++ ESPnet_streaming.py
@@ -4,6 +4,7 @@ import librosa
 from functools import lru_cache
 import time
 import argparse
+from espnet2.bin.asr_inference_streaming import Speech2Text
 
 buffer = []
 transcription = ""
@@ -23,21 +24,27 @@ def load_audio_chunk(fname, beg, end):
     return audio[beg_s:end_s]
 
 
-def initial_audio():
+def initial_audio(model):
     global buffer
     global transcription
-    text = transcribe(buffer)
+    text = transcribe(model,buffer)
     return text
 
-
+def transcribe(model,audio):
+    return model(audio)[0][0]
 
 parser = argparse.ArgumentParser()
 parser.add_argument('audio_path', type=str, help="Filename of 16kHz mono channel wav, on which live streaming is simulated.")
+parser.add_argument('asr_train_config',type=str,help="Filename of the training configuration file extension .yaml")
+parser.add_argument('asr_model_file',type=str,help="Filename of the model file extension .pth")
+parser.add_argument('--device',type=str,default='cuda',help="Device cpu/cuda default is cuda")
 args = parser.parse_args()
 
 logfile = sys.stderr
 
 audio_path = args.audio_path
+
+speech2text = Speech2Text(args.asr_train_config,args.asr_model_file,device=args.device)
 
 SAMPLING_RATE = 16000
 duration = len(load_audio(audio_path))/SAMPLING_RATE
@@ -46,6 +53,6 @@ print("Audio duration is: %2.2f seconds" % duration, file=logfile)
 for i in range(0,3):
     a = load_audio_chunk(audio_path,i,i+1)
     buffer.append(a)
-    t=initial_audio()
+    t=initial_audio(speech2text)
     transcription += t
     word_list.append(t)
