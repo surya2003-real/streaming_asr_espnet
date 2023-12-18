2b356e301fb74cafdcd5d093321929b38e304a8d
surya2003-real <sgoel1811@gmail.com>
2023-12-18 20:57:35 +0530

    minor bug fixes, basic streaming ASR ready

--- ESPnet_streaming.py
+++ ESPnet_streaming.py
@@ -6,7 +6,7 @@ import time
 import argparse
 from espnet2.bin.asr_inference import Speech2Text
 from new_conf_words import new_conf_words
-
+import os
 buffer = []
 transcription = ""
 conf_words=[]
@@ -45,27 +45,39 @@ args = parser.parse_args()
 logfile = sys.stderr
 
 audio_path = args.audio_path
-
+os.chdir('/home/suryansh/MADHAV/asr_train_asr_raw_hindi_bpe500')
 speech2text = Speech2Text(args.asr_train_config,args.asr_model_file,device=args.device)
 
 SAMPLING_RATE = 16000
 audio_len = len(load_audio(audio_path))
 duration = audio_len/SAMPLING_RATE
 print("Audio duration is: %2.2f seconds" % duration, file=logfile)
-
+start_time = time.time()
 curr_time=0
 for i in range(0,3):
+    start_time = time.time()
     a = load_audio_chunk(audio_path,i,i+1)
     txt = initial_audio(speech2text,a)
-    transcription += txt
+    transcription += " "+txt
     conf_words.append(txt)
+    print(transcription, time.time()-start_time)
 
 curr_time = 1
-while curr_time<audio_len:
-    a = load_audio_chunk(audio_path,curr_time,curr_time+3)
+while curr_time<duration:
+    start_time = time.time()
+    a = load_audio_chunk(audio_path,curr_time,min(curr_time+3, duration))
     txt = initial_audio(speech2text,a)
     curr_time += 1
     word_list = txt.split()
     conf_words,buffer,temp = new_conf_words(buffer,word_list,conf_words)
-    transcription += " ".join(temp)
-    print(transcription)
+    transcription += " "+" ".join(temp)
+    print(curr_time-1, min(curr_time+2, duration), transcription, time.time()-start_time)
+    delay = 1 - (time.time() - start_time)
+    if delay > 0:
+        time.sleep(delay)
+
+
+
+
+#Example command:
+#python3 ESPnet_streaming.py /home/suryansh/MADHAV/hindi_data/givenFolderHSp/finalSplitAudio/8janA_33.wav /home/suryansh/MADHAV/asr_train_asr_raw_hindi_bpe500/exp/asr_train_asr_raw_hindi_bpe500/config.yaml /home/suryansh/MADHAV/asr_train_asr_raw_hindi_bpe500/exp/asr_train_asr_raw_hindi_bpe500/valid.acc.ave_10best.pth
