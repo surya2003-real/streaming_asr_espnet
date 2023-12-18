916ad08477294682688c72feb21a8e906e703650
surya2003-real <sgoel1811@gmail.com>
2023-12-18 18:22:44 +0530

    conf_word_check added

---
+++ conf_word_check.py
@@ -0,0 +1,24 @@
+def conf_word_check(conf_words, word_list):
+    k=0
+    i=0
+    while(i < len(conf_words)):
+        if(conf_words[i]==word_list[k]):
+            flag=1
+            temp1=i
+            temp2=k
+            while(temp1<len(conf_words)-1 and temp2<len(word_list)-1):
+                temp1+=1
+                temp2+=1
+                if (conf_words[temp1]!=word_list[temp2]):
+                    flag=0
+                    break
+            if(flag):
+                i=temp1+1
+                k=temp2+1
+        i+=1
+    return k
+
+conf_words=['a','b','c','d','e','f','g','h','i','j','k','l','m','n']
+word_list=['k','l','m','n', 'o', 'p', 'q', 'r', 's']
+print(conf_word_check(conf_words, word_list), word_list[conf_word_check(conf_words, word_list)])
+    
