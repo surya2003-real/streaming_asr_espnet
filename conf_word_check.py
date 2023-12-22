ce981599ba4ef8e0bb7d2de680e2753ad2458a45
surya2003-real <sgoel1811@gmail.com>
2023-12-22 11:54:44 +0530

    bug fixed

--- conf_word_check.py
+++ conf_word_check.py
@@ -6,7 +6,7 @@ def conf_word_check(conf_words, word_list):
             if(conf_word==word_list[i]):
                 return i+1, l
         conf_word= conf_words[-2-l]
-    return 4
+    return 4, 0
 
 # conf_words=['a','b','c','d','e','f','g','h','i','j','k','l','m','n']
 # word_list=['k','l','m','n', 'o', 'p', 'q', 'r', 's']
