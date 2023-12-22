463898b79a59eee1442b07df5da9f49de551a929
surya2003-real <sgoel1811@gmail.com>
2023-12-22 11:12:56 +0530

    modified conf_word_check

--- conf_word_check.py
+++ conf_word_check.py
@@ -1,22 +1,10 @@
 def conf_word_check(conf_words, word_list):
-    k=0
-    i=0
-    while(i < len(conf_words) and k < len(word_list)):
-        if(conf_words[i]==word_list[k]):
-            flag=1
-            temp1=i
-            temp2=k
-            while(temp1<len(conf_words)-1 and temp2<len(word_list)-1):
-                temp1+=1
-                temp2+=1
-                if (conf_words[temp1]!=word_list[temp2]):
-                    flag=0
-                    break
-            if(flag):
-                i=temp1+1
-                k=temp2+1
-        i+=1
-    return k
+    conf_word= conf_words[-1]
+    modified_word_list=word_list[4:]
+    for i in range(len(modified_word_list)):
+        if(conf_word==modified_word_list[i]):
+            return i+5
+    return 4
 
 # conf_words=['a','b','c','d','e','f','g','h','i','j','k','l','m','n']
 # word_list=['k','l','m','n', 'o', 'p', 'q', 'r', 's']
