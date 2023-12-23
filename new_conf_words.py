c4e87ebdbacd6b05ed2c8a3da52c0d519ba6896f
Sankalp-Mittal <sankalpm22@iitk.ac.in>
2023-12-23 11:32:45 +0530

    make more robust

--- new_conf_words.py
+++ new_conf_words.py
@@ -2,8 +2,9 @@ from conf_word_check import conf_word_check
 def new_conf_words(buffer, word_list, conf_words):
     k, alarm=conf_word_check(conf_words, word_list)
     k+=alarm
+    ind = -max(alarm,4)
     if(len(word_list)==k):
-        return conf_words[-4:], [], []
+        return conf_words[ind:], [], []
     cnt=0
     for r in range(k,min(len(word_list), k+len(buffer))):
         if(word_list[r]==buffer[cnt]):
@@ -12,7 +13,7 @@ def new_conf_words(buffer, word_list, conf_words):
             break
     if cnt==0:
         cnt+=1
-    return conf_words[-4:]+word_list[k:k+cnt], word_list[k+cnt:], word_list[k:k+cnt]
+    return conf_words[ind:]+word_list[k:k+cnt], word_list[k+cnt:], word_list[k:k+cnt]
 
 # conf_words=['l','m','n']
 # buffer=['l', 'q']
