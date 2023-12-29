3b137398a505391d3c9a9258b7e36c076e45f538
Sankalp-Mittal <sankalpm22@iitk.ac.in>
2023-12-29 21:48:57 +0530

    fix bugs in datatype

--- stream_local.py
+++ stream_local.py
@@ -9,6 +9,36 @@ import os
 import sys
 import time
 from scipy.io.wavfile import write
+
+def pcm2float(sig, dtype='float32'):
+    """Convert PCM signal to floating point with a range from -1 to 1.
+    Use dtype='float32' for single precision.
+    Parameters
+    ----------
+    sig : array_like
+        Input array, must have integral type.
+    dtype : data type, optional
+        Desired (floating point) data type.
+    Returns
+    -------
+    numpy.ndarray
+        Normalized floating point data.
+    See Also
+    --------
+    float2pcm, dtype
+    """
+    sig = np.asarray(sig)
+    if sig.dtype.kind not in 'iu':
+        raise TypeError("'sig' must be an array of integers")
+    dtype = np.dtype(dtype)
+    if dtype.kind != 'f':
+        raise TypeError("'dtype' must be a floating point type")
+
+    i = np.iinfo(sig.dtype)
+    abs_max = 2 ** (i.bits - 1)
+    offset = i.min + abs_max
+    return (sig.astype(dtype) - offset) / abs_max
+
 # model options
 config_file = "/media/yawningwinner/New Volume/MADHAV_LAB/asr_train_asr_raw_hindi_bpe500/exp/asr_train_asr_raw_hindi_bpe500/config.yaml"
 model_file = "/media/yawningwinner/New Volume/MADHAV_LAB/asr_train_asr_raw_hindi_bpe500/exp/asr_train_asr_raw_hindi_bpe500/valid.acc.ave_10best.pth"
@@ -21,7 +51,7 @@ p = pa.PyAudio()
 
 CHANNELS = 1
 SAMPLE_RATE = 16000
-CHUNK = 1024
+CHUNK = 1000
 
 #Variable initializations
 buffer = []
@@ -31,43 +61,6 @@ conf_words=[]
 word_list=[]
 
 curr_time = 1
-
-# Callback function to read incoming data
-# def callback(in_data, frame_count, time_info, status):
-#     global buffer, transcription, conf_words, curr_time,prev_audio,initial_time
-#     audio_data = np.frombuffer(in_data, dtype=np.int16)
-#     if curr_time==1:
-#         txt = transcribe(model,audio_data)
-#         words = txt.split()
-#         conf_words += words[:-4]
-#         buffer += words[-4:]
-#         curr_time += 1
-#         transcription += " "+" ".join(conf_words)
-#         print(curr_time-1, curr_time+6, transcription)
-#         prev_audio = audio_data
-#     else:
-#         audio_data = prev_audio + audio_data
-#         audio_data = audio_data[16000:]
-#         # if(1-(time.time()-initial_time)>0):
-#         #     time.sleep(1-(time.time()-initial_time))
-#         start_time = time.time()
-#         # if a is None:
-#         #     print("break here")
-#         #     break
-#         txt = transcribe(model,audio_data)
-#         print(txt)
-#         curr_time += 1
-#         word_list = txt.split()
-#         print(word_list)
-#         word_list=word_list[:-1]
-#         conf_words,buffer,temp = new_conf_words(buffer,word_list,conf_words)
-#         if(len(temp)>0):
-#             transcription += " "+" ".join(temp)
-#         print(curr_time-1, curr_time+6, transcription, time.time()-start_time)
-#         prev_audio = audio_data
-#         initial_time = time.time()
-#     # print(audio_data.shape)
-#     return (in_data, pa.paContinue)
 
 stream = p.open(format=pa.paInt16,
                     channels=CHANNELS,
@@ -88,6 +81,7 @@ for _ in range(0, int(SAMPLE_RATE / CHUNK * 7)):
 
 # time.sleep(7)
 audio_data = np.frombuffer(b''.join(frames), dtype=np.int16)
+audio_data = pcm2float(audio_data)
 initial_time = time.time()
 txt = transcribe(model,audio_data)
 words = txt.split()
@@ -100,19 +94,23 @@ print(curr_time-1, curr_time+6, transcription)
 # Keep the program running
 while True:
     try:
+        new_frames = []
         # if(1-(time.time()-initial_time)>0):
         #     time.sleep(1-(time.time()-initial_time))
         for _ in range(0, int(SAMPLE_RATE / CHUNK * 1)):
             data = stream.read(CHUNK)
             frames.append(data)
-        audio_data = np.frombuffer(b''.join(frames), dtype=np.int16)
+            new_frames.append(data)
+        new_data = np.frombuffer(b''.join(new_frames), dtype=np.int16)
+        new_data = pcm2float(new_data)
+        audio_data=np.append(audio_data,new_data)
         print("audio_data: ", audio_data)
-        audio_data = audio_data[-int(SAMPLE_RATE/CHUNK)*7:]
+        transcription_data = audio_data[-int(SAMPLE_RATE/CHUNK)*7:]
         start_time = time.time()
         # if a is None:
         #     print("break here")
         #     break
-        txt = transcribe(model,audio_data)
+        txt = transcribe(model,transcription_data)
         print(txt)
         curr_time += 1
         word_list = txt.split()
@@ -149,33 +147,3 @@ print(audio_data)
 write(filename, sample_rate, audio_data.astype(np.int16))
 
 print(f"Audio data has been successfully saved to {filename}")
-        
-
-
-# try:
-#     while stream.is_active():
-#         if(1-(time.time()-initial_time)>0):
-#             time.sleep(1-(time.time()-initial_time))
-#         start_time = time.time()
-#         a = receive_audio_chunk()
-#         running_audio = a[curr_time*16000:]
-#         # if a is None:
-#         #     print("break here")
-#         #     break
-#         txt = transcribe(model,running_audio)
-#         print(txt)
-#         curr_time += 1
-#         word_list = txt.split()
-#         print(word_list)
-#         word_list=word_list[:-1]
-#         conf_words,buffer,temp = new_conf_words(buffer,word_list,conf_words)
-#         if(len(temp)>0):
-#             transcription += " "+" ".join(temp)
-#         print(curr_time-1, curr_time+6, transcription, time.time()-start_time)
-#         initial_time = time.time()
-# except KeyboardInterrupt:
-#     stream.stop_stream()
-#     print("stream stopped")
-# transcription += " "+" ".join(buffer)
-# print(transcription)
-        
